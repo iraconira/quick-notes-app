fa6c8c9274fc6632f23065bf75076e58e161cb64
iraklin <iraklimt@nebulab.com>
2022-08-27 18:24:53 +0200

    Initial setup
    Eslint, Prettier, Git, Specs and a default basic scafolding

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+    <main>
+        <HelloWorld msg="Hello With Secure!" />
+    </main>
+</template>
+
+<style scoped lang="scss">
+header {
+    line-height: 1.5;
+}
+</style>
